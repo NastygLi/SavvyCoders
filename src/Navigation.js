38b6814b25e81342985170c93a8925b4db045211
Dinko Karamfilov <DinkoKaramfilov@gmail.com>
2019-02-18 21:10:36 -0600

    added js navigation

---
+++ src/Navigation.js
@@ -0,0 +1,21 @@
+export default `
+
+<div id="nav">
+  <ul class="Container">
+    <li> <a class="facepic" href="index.html">Home</a></li>
+    <li> <a class="facepic" href="blog/index.html">Blog</a></li>
+    <li> <a class="facepic" href="https://www.linkedin.com/in/dinko-karamfilov-996019a1/">Linkedin</a></li>
+    <li> <a class="facepic" href="https://github.com/NastygLi">GitHuB</a></li>
+    <li> <a class="facepic" href="contact/index.html">Contact</a></li>
+    <li> <a class="facepic" href="Projects/index.html">Projects</a>
+      <ul>
+        <li>First</li>
+        <li>Second</li>
+        <li>Third</li>
+
+      </ul>
+    </li>
+
+</ul>
+</div >
+`;
